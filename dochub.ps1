a7753f3b2507a7d09c9761d89029c7375b9b9b51
Cameron Harp <charp5257@gmail.com>
2017-11-21 11:28:55 -0700

    Added config.json file and dochub.ps1

---
+++ dochub.ps1
@@ -0,0 +1,24 @@
+param([switch]$h, [switch]$help, [switch]$i, [switch]$init)
+
+if($h -or $help)
+{
+    Write-Host
+    Write-Host "Dochub Help"
+    Write-Host "---------------------"
+    Write-Host "  -i|-init      Initializes the project"
+    Write-Host "  -h|-help      Displays this view"
+    Write-Host
+
+    return
+}
+
+if($i -or $init)
+{
+    # would call C#
+
+    dotnet.exe build -c Release "tools\src\Dochub.Console"
+
+    dotnet.exe .\"tools\src\Dochub.Console\bin\Release\netcoreapp1.1\Dochub.Console.dll"
+
+    return
+}
